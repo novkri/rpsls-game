bc2959df4da32fa2f54a5641180a45ea714e8733
kris <nov.kri@yandex.ru>
2021-07-07 00:52:29 +0700

    game board start page

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,15 +1,35 @@
 <template>
-  <v-container>
-    Game Board
+  <!--  TODO: bg !! -->
+  <!--  TODO: container width ?-->
+  <v-container fluid class="game-container my-16 px-16" style="background-image: url('~@/assets/images/bg-triangle.svg') !important;">
+    <v-row class="mb-10">
+      <v-col class="d-flex justify-center" cols="6"><Cap capColor="blue" iconName="icon-paper.svg"></Cap></v-col>
+      <v-col class="d-flex justify-center" cols="6"><Cap capColor="red" iconName="icon-rock.svg"></Cap></v-col>
+    </v-row>
+
+    <v-row>
+      <v-col class="d-flex justify-center" cols="12">
+        <Cap capColor="yellow" iconName="icon-scissors.svg"></Cap>
+      </v-col>
+    </v-row>
+
+
   </v-container>
 </template>
 
 <script>
+import Cap from "@/components/widgets/Cap";
 export default {
-  name: "GameBoard"
+  name: "GameBoard",
+  components: {Cap},
+  data: () => ({
+    state: 'start'
+  })
 }
 </script>
 
 <style scoped>
-
+.game-container {
+  background-image: url("~@/assets/images/bg-triangle.svg") !important;
+}
 </style>
---
+++ src/components/widgets/Cap.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-card
+    elevation="1"
+    class="rounded-circle pa-8"
+    width="250"
+    height="250"
+    :color="capColor"
+    style="
+-webkit-box-shadow: 0px -11px 0px 0px rgba(12, 12, 12, 0.3) inset !important;
+-moz-box-shadow: 0px -11px 0px 0px rgba(12, 12, 12, 0.3) inset !important;
+box-shadow: 0px -11px 0px 0px rgba(12, 12, 12, 0.3) inset !important;"
+  >
+    <v-card
+      elevation="0"
+      class="rounded-circle d-flex justify-center align-center"
+      width="100%"
+      height="100%"
+      style="
+-webkit-box-shadow: 0px 11px 0px 0px rgba(112, 112, 112, 0.3) inset !important;
+-moz-box-shadow: 0px 11px 0px 0px rgba(112, 112, 112, 0.3) inset !important;
+box-shadow: 0px 11px 0px 0px rgba(112, 112, 112, 0.3) inset !important;"
+    >
+      <v-img
+        contain
+        width="100"
+        height="100"
+        :src="require(`@/assets/images/${iconName}`)"
+      ></v-img>
+    </v-card>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Cap",
+  props: {
+    capColor: String,
+    iconName: String
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
