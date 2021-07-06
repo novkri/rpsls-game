72b71d2fc6818589f599c12fbe166f6433936788
kris <nov.kri@yandex.ru>
2021-07-07 00:03:34 +0700

    rules modal

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <v-container>
     Game Board
-  </div>
+  </v-container>
 </template>
 
 <script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <v-container>
     <v-card class="info-card rounded-xl pa-6">
-      <!--TODO: styles-->
       <v-row>
         <v-col>
           <p class="white--text text-h3 text-uppercase font-weight-bold">
@@ -12,15 +11,18 @@
         </v-col>
         <v-spacer></v-spacer>
         <v-col>
-          <v-card width="270"
+          <v-card
+            width="270"
             class="fill-height rounded-lg pa-4 d-flex flex-column justify-center align-center ml-auto"
           >
             <v-card-title
               class="pa-0 text-uppercase text-h6 font-weight-bold"
-              style="letter-spacing: 2px !important; color: hsl(229, 64%, 46%);"
+              style="letter-spacing: 3px !important; color: hsl(229, 64%, 46%);"
               >score</v-card-title
             >
-            <v-card-title class="pa-0 text-h1 font-weight-bold" style="color: hsl(0,0%,39%) !important;"
+            <v-card-title
+              class="pa-0 text-h1 font-weight-bold"
+              style="color: hsl(0,0%,39%) !important;"
               >12</v-card-title
             >
           </v-card>
@@ -32,9 +34,8 @@
     <game-board></game-board>
 
     <v-row>
-      <v-col>
-        <!--        TODO: modal-->
-        <v-btn>Rules</v-btn>
+      <v-col align="end">
+        <Modal />
       </v-col>
     </v-row>
   </v-container>
@@ -42,10 +43,18 @@
 
 <script>
 import GameBoard from "@/components/GameBoard";
+import Modal from "@/components/Modal";
 export default {
   name: "Home",
-  components: { GameBoard },
-  data: () => ({})
+  components: {Modal, GameBoard },
+  data: () => ({
+    isRulesOpen: false,
+  }),
+  methods: {
+    toggleRules() {
+      this.isRulesOpen = !this.isRulesOpen
+    }
+  }
 };
 </script>
 
@@ -53,5 +62,10 @@ export default {
 .info-card {
   border: 4px solid hsl(217, 16%, 45%) !important;
   background-color: transparent !important;
+}
+
+.rules-btn {
+  background-color: transparent !important;
+  border: 2px solid #d5d5d5;
 }
 </style>
