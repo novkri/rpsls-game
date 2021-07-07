72521f11fb29b595b55f0a12b5eb7aa76f8dfd23
kris <nov.kri@yandex.ru>
2021-07-07 07:55:44 +0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,4 @@ git commit -m 'deploy'
 # если вы публикуете по адресу https://<USERNAME>.github.io
 git push -f git@github.com:novkri/rpsls-game.git master
 
-# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
 cd -
