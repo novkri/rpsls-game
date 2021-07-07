08be3ae62aa87ee5cd05ee754071d5a8a75fe276
kris <nov.kri@yandex.ru>
2021-07-07 07:57:58 +0700

    publicPath changed

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: process.env.NODE_ENV === "production" ? "/rpsls-game.io/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/rpsls-game/" : "/"
 };
